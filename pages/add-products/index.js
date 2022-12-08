80c8b5cf92f720886f163592293cabd9e0c00c10
Kritib Bhattarai <kritibbhattarai@Kritibs-MacBook-Pro.local>
2022-12-08 03:09:57 -0600

    fixed add

--- pages/add-products/index.js
+++ pages/add-products/index.js
@@ -1,10 +1,20 @@
-import Add from "../../components/Add/Add.js";
+import {AddProducts} from "../../components/Shop";
+import {useRouter} from "next/router";
+import { fetcher } from "../../fetch/";
 import useSWR from "swr";
 
-export default function addproducts() {
+export default function Addproducts() {
+		const router = useRouter();
+		const { data, error } = useSWR([`products/${router.query.id}`, "GET"], fetcher);
+	if (!data) return "I am loading";
+	if (error) return "there is error";
 	return (
 
-		<Add/>
+		router.query.id?
+		<AddProducts data={data} />
+			:
+		<AddProducts data='' />
+
 	
 	);
 	}
